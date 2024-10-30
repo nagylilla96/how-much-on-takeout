4a5ddc2aec06588ab19402bbc2bbad9e31b52f7c
lillanagy <n.lilla96@gmail.com>
2024-10-30 21:01:45 +0200

    Renamed Big Belly cookie parameter

--- merchants/big_belly.py
+++ merchants/big_belly.py
@@ -2,20 +2,20 @@ import requests
 import re
 # TODO: remove this, only added for testing
 import io
-from config import BASE_URL_BIG_BELLY, BIG_BELLY_AUTH_TOKEN
+from config import BASE_URL_BIG_BELLY, BIG_BELLY_COOKIE
 try: 
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
 
 def fetch_big_belly_orders():
-    if not BIG_BELLY_AUTH_TOKEN:
-        print("BIG_BELLY_AUTH_TOKEN is missing.")
+    if not BIG_BELLY_COOKIE:
+        print("BIG_BELLY_COOKIE is missing.")
         return 0.0, 0
 
     total_sum = 0.0
     order_count = 0
-    headers = {"Cookie": BIG_BELLY_AUTH_TOKEN}
+    headers = {"Cookie": BIG_BELLY_COOKIE}
 
     response = requests.get(BASE_URL_BIG_BELLY, headers=headers)
     with io.open("output.txt", mode="a", encoding="utf-8") as f:
